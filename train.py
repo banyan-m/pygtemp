3b5429c332636e1f87934aa21f1b8784274a67e9
Michael <Michaeltdearmas@gmail.com>
2024-07-04 21:29:59 +0000

    working on train

--- train.py
+++ train.py
@@ -10,3 +10,73 @@ import pandas as pd
 import torch
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+def count_parameters(model):
+    return sum(p.numel() for p in model.parameters() if p.requires_grad)
+
+
+def train_one_epoch(epoch,model,train_loader,optimizer,loss_fn):
+
+    all_preds = []
+    all_labels = []
+    running_loss = 0.0
+    steps = 0
+
+    for _, batch in enumerate(tqdm(train_loader)):
+        batch = batch.to(device)
+        optimizer.zero_grad()
+
+        pred = model(batch.x.float(),
+                     batch.edge_attr.float(),
+                     batch.edge_index,
+                     batch.batch)
+        
+        loss = loss_fn(torch.squeeze(pred), batch.y.float())
+        loss.backward()
+        optimizer.step()
+        running_loss += loss.item()
+        steps += 1
+
+        all_preds.append(np.rint(torch.sigmoid(pred).detach().cpu().numpy()))
+        all_labels.append(batch.y.detach().cpu().numpy())
+
+    all_preds = np.concatenate(all_preds).ravel()
+    all_labels = np.concatenate(all_labels).ravel()
+    calculate_metrics(all_preds, all_labels,epoch, "train")
+    return running_loss / steps
+
+
+def test(epoch,test_loader, loss_fn):
+    all_preds = []
+    all_preds_raw = []
+    all_labels = []
+    running_loss = 0.0
+    step = 0
+
+    for batch in test_loader:
+        batch.to(device)
+        pred = model(batch.x.float(),
+                     batch.edge_attr.float(),
+                     batch.edge_index,
+                     batch.batch)
+        
+        loss = loss_fn(torch.squeeze(pred), batch.y.float())
+        
+        running_loss += loss.item()
+        steps += 1
+
+        all_preds.append(np.rint(torch.sigmoid(pred).detach().cpu().numpy()))
+        all_preds_raw.append(torch.sigmoid(pred).detach().cpu().numpy())
+        all_labels.append(batch.y.detach().cpu().numpy())
+
+    all_preds = np.concatenate(all_preds).ravel()
+    all_labels = np.concatenate(all_labels).ravel()
+
+    print(all_preds_raw[0][:10])
+    print(all_preds[:10])
+    print(all_labels[:10])
+    calculate_metrics(all_preds, all_labels, epoch, "test")
+    log_conf_matrix(all_preds, all_labels, epoch)
+    
+    return running_loss / steps
+    
